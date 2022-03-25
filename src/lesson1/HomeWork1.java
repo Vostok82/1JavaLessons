10b3991cefec53131bf9f2e54b7c7e3ae2ab91e2
Vostok82 <82romanpavlov@gmail.com>
2022-03-25 16:39:35 +0200

    Выполнил первое ДЗ

---
+++ src/lesson1/HomeWork1.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class HomeWork1 {
+
+    public static void main(String[] args) {
+        foo();
+    }
+
+    private static void foo(){
+        System.out.println("Решение первой задачи");
+    }
+}
