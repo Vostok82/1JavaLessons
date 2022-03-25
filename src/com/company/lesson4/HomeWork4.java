ad7a5b75452b340d33e4e0253f80c48d0aa74e56
Vostok82 <82romanpavlov@gmail.com>
2022-03-25 19:31:05 +0200

    ДЗ 4 правки

--- src/com/company/lesson4/HomeWork4.java
+++ src/com/company/lesson4/HomeWork4.java
@@ -3,6 +3,6 @@ package com.company.lesson4;
 public class HomeWork4 {
 
     public static void main(String[] args) {
-        System.out.println("HW4");
+        System.out.println("HW4!!!");
     }
 }
